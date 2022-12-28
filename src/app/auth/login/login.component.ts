d9230eccc8066d3fd738b8d5373b266711dd12ec
hector.romang <hector.romang@durango.gob.mx>
2022-12-28 17:01:20 -0600

    Login

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,20 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: [ './login.component.css' ]
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor( private router: Router ) { }
+  public formSubmitted: boolean = false;
 
-  ngOnInit(): void {
-  }
+  public loginForm = this.fb.group({
+    email: [ 'tests@gmail.com', [Validators.required, Validators.email]],
+    password: [ '123456', Validators.required ],
+    remember: [false]
+  });
+
+  constructor( private router: Router,
+               private fb: FormBuilder,
+               private loginService: UsuarioService ) { }
 
   login(): void {
-    this.router.navigateByUrl('/');
+
+    this.loginService.loginUsuario( this.loginForm.value ).subscribe(res => {
+      console.log(res)
+    },err => {
+      Swal.fire(
+        'Error',
+        err.error.msg,
+        'error'
+      )
+    })
+
+    // console.log( this.loginForm.value );
+
+
+    // this.router.navigateByUrl('/');
   }
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RegisterForm } from '../interfaces/register-form.interface.';
 import { environment } from 'src/environments/environment';
+import { LoginForm } from '../interfaces/login-form.interface';
 
 const base_url = environment.base_url;
 
@@ -17,5 +18,13 @@ export class UsuarioService {
     return this.http.post(`${base_url}/usuarios`, formData)
 
   }
+
+  loginUsuario( formData: LoginForm ) {
+
+    return this.http.post(`${base_url}/login`, formData)
+
+  }
+
+
 
 }
